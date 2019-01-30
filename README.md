3415bf1e5b8b9fae4445c71aa2286b54d8d1b5ff
Robert Cope <robert.cope@solidfire.com>
2019-01-30 14:54:47 -0700

    Commiting the pracite shell script and the lab info.

---
+++ README.md
@@ -0,0 +1,2 @@
+# csci3308_lab_2
+Lab 2 for CSCI 3308 at CU Boulder
---
+++ practice.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# set -eu
+echo "Enter a number: "
+read numOne
+echo "Enter a second number: "
+read numTwo
+sum=$((${numOne} + ${numTwo}))
+echo "The sum is : ${sum}"
+let prod=numOne*numTwo
+echo "The product is: ${prod}"
+
+echo "File Name: $0"
+echo "Command Line Argument 1: $1"
+grep $1 $2
